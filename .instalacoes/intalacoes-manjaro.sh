eae5eb66ad96f323dac0c1e3306a0d3707e92235
Natália Fonseca <nataliabf@outlook.com>
2022-03-05 21:02:42 -0300

    update dotfiles

--- .instalacoes/intalacoes-manjaro.sh
+++ .instalacoes/intalacoes-manjaro.sh
@@ -15,9 +15,7 @@ sudo pacman -Syu yay
 yay -Syu --noconfirm make
 
 # sublime
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-yay -Syu --noconfirm sublime-text
+yay -Syu --noconfirm sublime-text-4
 
 # code
 yay -Syu --noconfirm visual-studio-code-bin
@@ -27,6 +25,9 @@ yay -Syu --noconfirm telegram-desktop
 
 # Notion
 yay -Syu --noconfirm notion-app-enhanced
+
+# Docker
+yay -Syu --noconfirm docker
 
 # pip / pipx / poetry
 yay -Syu --noconfirm python-pip
